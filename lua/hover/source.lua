fa631a48835a1ab350a55a47ae3ac6a45c64e5b0
dousbao <dousbao.cjx@gmail.com>
2024-01-28 10:00:46 -0800

    init: basic sources management

---
+++ lua/hover/source.lua
@@ -0,0 +1,33 @@
+local M = {}
+local sources = {}
+
+function M.add(source)
+	vim.validate({
+		name = { source.name, "string" },
+		priority = { source.priority, "number" },
+		enabled = { source.enabled, { "boolean", "number" } },
+		fetch = { source.fetch, "function" }
+	})
+
+	-- existence check + ordered insert
+	local pos = -1
+	for i, s in ipairs(sources) do
+		if source.name == s.name then
+			error("trying to add source with name already exist: " .. source.name)
+		elseif pos == -1 and source.priority < s.priority then
+			pos = i
+		end
+	end
+
+	if pos ~= -1 then
+		table.insert(sources, pos, source)
+	else
+		table.insert(sources, source)
+	end
+end
+
+function M.snapshot()
+	return vim.deepcopy(sources)
+end
+
+return M
